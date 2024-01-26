345db746610755163a734f62fe002029d94b1ade
Amael <lounkokobia@gmail.com>
2024-01-26 22:49:44 +0100

    Exo 0

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -8,7 +8,14 @@ of operations needed to result in exactly n H characters in the file.
 Function:
    def minOperations(n)
 """
-
+def is_prime(n):
+    """
+    Is a prime number
+    """
+    for num in range(2, n):
+        if n % num == 0:
+            return False
+    return True
 
 def minOperations(n):
     """
@@ -17,13 +24,12 @@ def minOperations(n):
 
     Args:
        n:number of operations
-    """
-    
+    """    
     if n <= 1:
         return 0
     else:
         for num in range(n-1, 2, -1):
-            if n % num == 0:
+            if n % num == 0 and is_prime(num):
                 return int((n / num) + num)
         else:
             return n
