bceda08ac08a1399a3bc722ea8080e36c8cb77e8
Amael <lounkokobia@gmail.com>
2024-01-26 16:47:18 +0100

    Exo 0

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+This module contains :
+
+Given a number n, write a method that calculates the fewest number
+of operations needed to result in exactly n H characters in the file. 
+
+Function:
+   def minOperations(n)
+"""
+
+
+def minOperations(n)
+    """
+    Given a number n, write a method that calculates the fewest number
+    of operations needed to result in exactly n H characters in the file. 
+    
+    Args:
+       n:number of operations
+    """
+    
+
+    return n_op 
+
