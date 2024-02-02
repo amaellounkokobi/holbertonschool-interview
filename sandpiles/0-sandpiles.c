2b81474c53af2a9a0d3cbfc8cde7887942711701
Amael <lounkokobia@gmail.com>
2024-02-02 18:28:47 +0100

    sandpile

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -7,6 +7,40 @@
  * @grid2: Right 3x3 grid
  *
  */
+
+
+int is_stable(int grid1[3][3]){
+
+  int i, j;
+  
+  for (i = 0; i < 3; i++)
+    {
+      for (j = 0; j < 3; j++)
+        {
+	  if (grid1[i][j] > 3){
+	    return(0);
+	  }
+        }
+    }
+  return(1);
+}
+
+void show_grid(int grid[3][3])
+{
+  int i, j;
+  
+  for (i = 0; i < 3; i++)
+    {
+      for (j = 0; j < 3; j++)
+	{
+	  if (j)
+	    printf(" ");
+	  printf("%d", grid[i][j]);
+	}
+      printf("\n");
+    }
+}
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]){
 
   int i, j;
@@ -24,6 +58,10 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3]){
   /* Sand pile */    
   while(is_stable(grid1) == 0)
     {
+      
+      printf("=\n");
+      show_grid(grid1);
+      
       for (i = 0; i < 3; i++)
 	{
 	  for (j = 0; j < 3; j++)
@@ -31,48 +69,33 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3]){
 	      
 	      if(grid1[i][j] > 3)
 		{
+		 
 		  grid1[i][j] = grid1[i][j] - 4;  
-	      
+
+		  if(grid1[i][j] < 0 )
+		    grid1[i][j] = 0;  
+		  
 		  /* Check adjacent grid and pile */
 
 		  /* bottom */
-		  if(grid1[i + 1][j])
-		    grid1[i + 1][j]++;
+		  if(i + 1 >= 0 && i + 1 < 3)
+		    grid1[i + 1][j] += 1;
 
 		  /* top */
-		  if(grid1[i - 1][j])
-		    grid1[i - 1][j]++;
+
+		  if(i - 1 >= 0 && i - 1 < 3)
+		    grid1[i - 1][j] += 1;
 		  
 		  /* left */
-		  if(grid1[i][j + 1])
-		    grid1[i][j + 1]++;
+		  if(j + 1 >= 0 && j + 1 < 3)
+		    grid1[i][j + 1] += 1;
 
 		  /* right */
-		  if(grid1[i][j - 1])
-		    grid1[i][j - 1]++;
+		  if(j - 1 >= 0 && j - 1 < 3)
+		    grid1[i][j - 1] += 1;
 		}
 	    }
 	}
-      
-      printf("=\n");
-      print_grid(grid1);
+
     }
-}
-
-
-
-int is_stable(int grid){
-
-  int i, j;
-  
-  for (i = 0; i < 3; i++)
-    {
-      for (j = 0; j < 3; j++)
-        {
-	  if (grid[i][j] > 3){
-	    return(0);
-	  }
-        }
-    }
-  return(1);
 }
