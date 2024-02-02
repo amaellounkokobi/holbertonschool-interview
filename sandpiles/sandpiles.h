006c0122db98b5ae76e57e1d501d16073fe6c162
Amael <lounkokobia@gmail.com>
2024-02-02 14:44:19 +0100

    sandpiles.h

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,19 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+#include <stddef.h>
+
+/**
+ * 
+ * sandepiles_sum - Print 3x3 grids sum
+ * 
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ *
+ * Description: computes the sum of two sandpiles
+ * for Holberton project
+ */
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* SANDPILES_H */
