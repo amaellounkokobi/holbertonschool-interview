bab91f72498425a323fd0dc9e37585a4a26a596e
Amael <lounkokobia@gmail.com>
2024-02-02 14:51:12 +0100

    Exo 0

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -11,12 +11,6 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3]){
 
   int i, j;
 
-  int grid[3][3] = {
-    {0, 0, 0},
-    {0, 0, 0},
-    {0, 0, 0}
-  };
-
     for (i = 0; i < 3; i++)
     {
         for (j = 0; j < 3; j++)
