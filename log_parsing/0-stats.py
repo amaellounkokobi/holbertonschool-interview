43f7016d1a9145ddaf4f0a37d3232e757557910d
Amael <lounkokobia@gmail.com>
2024-02-12 15:17:48 +0100

    Exo 0

---
+++ log_parsing/0-stats.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+This module contains :
+A script that reads stdin line by line and computes metrics:
+   # Read STDIN
+   # Get the line ( or lines)
+   # Print statistics
+   # Print computed file size
+   # File size: <total size>
+   # Print status codes
+   # <status code>: <number>
+
+"""
+import sys
+
+
+def parse_lines(lines_array):
+    """
+    Parsing array return results
+
+    Args:
+    lines_array lines to parse
+    """
+    file_size = 0
+    status_list = []
+    status_tab = [200,
+                  301,
+                  400,
+                  401,
+                  403,
+                  404,
+                  405,
+                  500]
+
+    for line in lines_array:
+        infos = line.split(" ")
+        file_size += int(infos[7])
+        status_list.append(infos[8])
+
+    print("File size:", file_size)
+
+    for status in status_tab:
+        counter = status_list.count(status)
+        if counter != 0:
+            print('{}: {}'.format(status, counter))
+
+# Array that will containes lines
+buffer_array = []
+
+try:
+    for line in sys.stdin:
+        # Read STDIN get the lines
+        line = line.rstrip()
+        buffer_array.append(line)
+
+        if len(buffer_array) == 10:
+            parse_lines(buffer_array)
+            buffer_array.clear()
+
+except KeyboardInterrupt:
+    parse_lines(buffer_array)
